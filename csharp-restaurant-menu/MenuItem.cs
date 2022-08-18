420d32188393ec1da901a3a1944175d4ba111cca
codeswitchingstl <codeswitching@icloud.com>
2022-08-18 17:03:18 -0500

    updated commit after blue screen on computer during studio

---
+++ csharp-restaurant-menu/MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    internal class MenuItem
+    {
+        public MenuItem(double price, string name, string description, string category, bool isNew, DateTime dateCreated)
+        {
+            Price = price;
+            Name = name;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+            DateCreated = dateCreated;
+        }
+
+        public double Price {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ csharp-restaurant-menu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_restaurant_menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
